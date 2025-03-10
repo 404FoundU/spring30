a8c391072726afe2a1532b8d924c0141a76f0ef5
404FoundU <unnikrishnanrp@outlook.com>
2025-03-10 12:15:01 -0500

    websocket

--- saga-order-payment/order-service/src/main/java/com/example/order/OrderServiceApplication.java
+++ saga-order-payment/order-service/src/main/java/com/example/order/OrderServiceApplication.java
@@ -3,6 +3,8 @@ package com.example.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//choreography pattern
 @SpringBootApplication
 public class OrderServiceApplication {
     public static void main(String[] args) {
